e2e2b4478b35b738643e9e1dad18a3183e0c0f47
kadyrgulov-artur <arturkadyrgulov@gmail.com>
2023-01-20 21:17:59 +0500

    Add simple modle for mvc pattern

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
- ], function (Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+ ], function (Controller, MessageToast, JSONModel) {
     "use strict";
 
     // Controller names are capitalized, for example App.controller.js
@@ -15,8 +16,18 @@ sap.ui.define([
     // sap.ui.demo.walkthrough.controller.App specified in App.view.xml like controller for some logic
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+        onInit : function () {
+            // set data model on view
+            var oData = {
+               recipient : {
+                  name : "World"
+               }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
         onShowHello : function () {
             MessageToast.show("Hello World")
-       }
+        }
     });
  });
